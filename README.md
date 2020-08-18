c87cb0da6f3a87d924685363b25ec3496832a5fc
daicalong <hpnguyen52@gmail.com>
2020-08-17 22:21:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nfl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nfl)
---
+++ src/app/player-list/player-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { players } from '../players.constant';
+
+@Component({
+  selector: 'app-player-list',
+  templateUrl: './player-list.component.html',
+  styleUrls: ['./player-list.component.css']
+})
+export class PlayerListComponent {
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
