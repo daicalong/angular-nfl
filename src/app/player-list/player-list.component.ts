00dd568005fb78163a7d9a9d5881ec940e2417aa
Harry Nguyen <hpnguyen52@gmail.com>
2020-08-17 22:49:27 -0400

    fixed a few things

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
-
-import { players } from '../players.constant';
+import { Component, OnInit } from '@angular/core';
+import { players } from '../constants/players.constant';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
   styleUrls: ['./player-list.component.css']
 })
-export class PlayerListComponent {
+
+export class PlayerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
